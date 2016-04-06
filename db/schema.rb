1f88121c4c6b34eee4073f83e0df8ea172e2e7c1
Gemma Gotch <pveggie@hotmail.com>
2016-04-06 17:22:00 +0200

    switching to master to check things

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160406141122) do
+ActiveRecord::Schema.define(version: 20160406150725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
