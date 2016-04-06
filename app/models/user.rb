dc0501da82462dade682aed4ac8059bd72d438dc
Thibaud Godet <thibaud.godet@gmail.com>
2016-04-06 09:47:17 +0200

    new banners

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :books
+
   PROFILES_PICS = ["profilepic_1.jpg","profilepic_2.jpg","profilepic_3.jpg","profilepic_4.jpg",
     "profilepic_5.jpg","profilepic_6.jpg","profilepic_7.jpg","profilepic_8.jpg",
     "profilepic_9.jpg","profilepic_10.jpg","profilepic_11.jpg","profilepic_12.jpg",
