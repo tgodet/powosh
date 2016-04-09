e9c9295daa82b4752fa61db056ac7438331c4995
Gemma Gotch <pveggie@hotmail.com>
2016-04-09 14:55:47 +0200

    devise setup done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
+  layout 'home'
+  skip_before_action :authenticate_user!, only: :home
+
   def home
+    # user taken to books index as soon as they sign in.
+    redirect_to books_path if user_signed_in?
   end
 end
