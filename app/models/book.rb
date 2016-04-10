ff437b31201364457c7f5cf66b82e382291c546c
Gemma Gotch <pveggie@hotmail.com>
2016-04-10 13:35:08 +0200

    class methods in book modal for showing own books and friends books

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,12 @@ class Book < ActiveRecord::Base
   validates :author, presence:true
 
   LANGUAGES = ["Englais", "Français", "Néerlandais", "Allemand", "Espagnol", "Portugais", "Other"]
+
+  def self.of_user(user_id)
+    Book.where(user_id: user_id)
+  end
+
+  def self.of_friends(user_id)
+    Book.joins(user: :friendships).where("friendships.friend_id = '#{user_id}'")
+  end
 end
