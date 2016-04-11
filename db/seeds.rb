55287f4533bc672c17b8ae51fe3a96b37b70aa53
Thibaud Godet <thibaud.godet@gmail.com>
2016-04-11 16:42:27 +0200

    new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,51 +7,51 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Loan.delete_all
 Book.delete_all
-# Profile.delete_all
+Profile.delete_all
 Friendship.delete_all
-# User.delete_all
+User.delete_all
 
-# User.create!(email: "test1@test.com", password: "testtest")
-# User.create!(email: "test2@test.com", password: "testtest")
-# User.create!(email: "test3@test.com", password: "testtest")
-# User.create!(email: "test4@test.com", password: "testtest")
-# User.create!(email: "test5@test.com", password: "testtest")
-# User.create!(email: "test6@test.com", password: "testtest")
+User.create!(email: "test1@test.com", password: "testtest")
+User.create!(email: "test2@test.com", password: "testtest")
+User.create!(email: "test3@test.com", password: "testtest")
+User.create!(email: "test4@test.com", password: "testtest")
+User.create!(email: "test5@test.com", password: "testtest")
+User.create!(email: "test6@test.com", password: "testtest")
 
-# profile_attributes = [
-#   {
-#     user_id: (User.find_by email: "test1@test.com").id,
-#     first_name: "Percival",
-#     profile_picture: "profilepic_1.jpg"
-#     },
-#   {
-#     user_id: (User.find_by email: "test2@test.com").id,
-#     first_name: "Mario",
-#     profile_picture: "profilepic_2.jpg"
-#     },
-#   {
-#     user_id: (User.find_by email: "test3@test.com").id,
-#     first_name: "Gary",
-#     profile_picture: "profilepic_3.jpg"
-#     },
-#   {
-#     user_id: (User.find_by email: "test4@test.com").id,
-#     first_name: "Sturm",
-#     profile_picture: "profilepic_4.jpg"
-#   },
-#     {
-#     user_id: (User.find_by email: "test5@test.com").id,
-#     first_name: "Jerry",
-#     profile_picture: "profilepic_5.jpg"
-#   },
-#     {
-#     user_id: (User.find_by email: "test6@test.com").id,
-#     first_name: "Pete",
-#     profile_picture: "profilepic_6.jpg"
-#   }
-# ]
+profile_attributes = [
+  {
+    user_id: (User.find_by email: "test1@test.com").id,
+    first_name: "Percival",
+    profile_picture: "profilepic_1.jpg"
+    },
+  {
+    user_id: (User.find_by email: "test2@test.com").id,
+    first_name: "Mario",
+    profile_picture: "profilepic_2.jpg"
+    },
+  {
+    user_id: (User.find_by email: "test3@test.com").id,
+    first_name: "Gary",
+    profile_picture: "profilepic_3.jpg"
+    },
+  {
+    user_id: (User.find_by email: "test4@test.com").id,
+    first_name: "Sturm",
+    profile_picture: "profilepic_4.jpg"
+  },
+    {
+    user_id: (User.find_by email: "test5@test.com").id,
+    first_name: "Jerry",
+    profile_picture: "profilepic_5.jpg"
+  },
+    {
+    user_id: (User.find_by email: "test6@test.com").id,
+    first_name: "Pete",
+    profile_picture: "profilepic_6.jpg"
+  }
+]
 
-# profile_attributes.each { |params| Profile.create!(params) }
+profile_attributes.each { |params| Profile.create!(params) }
 
 rand_friend_1 = User.all.sample
 rand_friend_2 = User.all.sample
